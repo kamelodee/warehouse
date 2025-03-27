48c02e689c1667cd28fcdfe04ecf62b073a33a89
kamelodee <kamelodee@gmail.com>
2025-03-27 18:00:50 +0000

    changes made now

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -10,7 +10,7 @@ export default function LoginPage() {
   const [error, setError] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
-
+// jewhrer
   const handleLogin = async () => {
     setError('');
     setIsLoading(true);
